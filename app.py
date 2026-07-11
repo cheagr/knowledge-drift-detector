f0bc699d0d03d81592f78ff1a9015b6e3e6b8b13
Chetan <cagrawal11@gmail.com>
2026-07-11 16:28:51 +0530

    Introduced artifact validator to do checks on uploaded files before calling llm

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from confluence_parser import parse_confluence
 from ado_parser import parse_ado
 from gemini_client import compare_documents
 from feature_resolver import resolve_feature
+# from artifact_validator import validate_artifacts
+from artifact_validator import (validate_uploads, validate_selected_feature)
 
 st.set_page_config(
     page_title="Knowledge Drift Detector",
@@ -55,55 +57,34 @@ if confluence_file and ado_file:
         st.error(ado["error"])
         st.stop()
 
-    ##########################################
-    # Validation Passed
-    ##########################################
-
-    st.success("Both artifacts parsed successfully.")
-
     st.divider()
-    # with st.spinner("Generating AI comparision report"):
-    #     success, drift = compare_documents(confluence,ado)
 
-    # if success:
-    #     st.subheader("AI Compare ")
-    #     st.subheader("Executive Summary:")
-    #     st.write(drift["executive_summary"])
-            
-    #     st.subheader("⚠ Knowledge Drifts")
-    #     for knowledge_drift in drift["knowledge_drifts"]:
-    #         # st.write(f"• {knowledge_drift{}}")
-    #         st.write(f"**Area** -> {knowledge_drift['area']}")
-    #         st.write(f"• {knowledge_drift['confluence']}")
-    #         st.write(f"• {knowledge_drift['ado']}")
-    #         st.write(f"**Impact** -> {knowledge_drift['impact']}")
-            
-    #         st.divider()
+    ########################################
+    # Validate the uploaded artifacts
+    #######################################
 
-    #     st.subheader("✅ Suggested reviews for PM")
-    #     for review in drift["manual_review"]:
-    #         st.write(f"• {review}")
+    success, validation = validate_uploads(
+        confluence,
+        ado
+    )
 
-    #     quality = drift["analysis_quality"]
+    if validation["errors"]:
+        st.error("Artifact validation failed.")
 
-    #     st.subheader("📊 Analysis Quality")
+        for error in validation["errors"]:
+            st.error(error)
 
-    #     st.metric(
-    #         "Confidence",
-    #         quality["confidence"]
-    #     )
+        st.stop()
 
-    #     st.caption(quality["reason"])
-    # else:
-    #     st.error(f"Failed to generate AI summary. Error: {drift['error']}")
-    
-    # st.subheader("Raw data")
-    # with st.expander("Structured json output from llm"):
-    #     st.json(drift)
+    if validation["warnings"]:
 
+        with st.expander("Artifact Validation Warnings"):
+
+            for warning in validation["warnings"]:
+                st.warning(warning)
 
     ##########################################
-    # Resolve Feature
+    # Resolve Feature to find match against the confluence
     ##########################################
 
     success, match = resolve_feature(
@@ -116,11 +97,9 @@ if confluence_file and ado_file:
         st.error("Unable to match feature.")
         st.stop()
 
-    st.success("Artifacts parsed successfully.")
+    st.success("Both Artifacts parsed successfully. ADO hierarchy built")
 
     st.divider()
-
-    col1, col2 = st.columns(2)
 
     ##########################################
     # Selecting ADO Feature based on match confidence
@@ -140,8 +119,15 @@ if confluence_file and ado_file:
     else:
 
         st.warning(
-            "Unable to confidently identify the correct feature."
-        )
+            f"""
+            Multiple potential ADO Features matching the Confluence were found.
+
+            The best automatic match had **{match['confidence']} confidence**
+            (score: {match['score']}).
+
+            Please select the correct feature before continuing.
+            """
+            )
 
         options = []
 
@@ -160,30 +146,98 @@ if confluence_file and ado_file:
 
         selected = st.radio(
             "Select the correct ADO Feature",
-            options
+            options,
+            index=None
         )
 
+        if selected is None:
+            st.stop()
+
         feature = lookup[selected]
+
+    ##################################################
+    # Validate stories under the selected feature
+    ##################################################
+
+    success, validation = validate_selected_feature(feature)
+
+    if validation["errors"]:
+
+        st.error("Selected Feature's validation failed.")
+
+        for error in validation["errors"]:
+            st.error(error)
+
+        st.stop()
+
+    if validation["warnings"]:
+
+        with st.expander("Selected Feature Validation Warnings"):
+
+            for warning in validation["warnings"]:
+                st.warning(warning)
+        
+    ##########################################
+    # Call LLM with confluence and selected feature 
+    # Display AI report
+    ##########################################
+
+    with st.spinner("Generating AI comparision report"):
+        success, drift = compare_documents(confluence,feature)
+
+    if success:
+        st.subheader("AI Compare ")
+        st.subheader("Executive Summary:")
+        st.write(drift["executive_summary"])
+            
+        st.subheader("⚠ Knowledge Drifts")
+        for knowledge_drift in drift["knowledge_drifts"]:
+            # st.write(f"• {knowledge_drift{}}")
+            st.write(f"**Area** \n {knowledge_drift['area']}")
+            st.write(f" • {knowledge_drift['confluence']}")
+            st.write(f"• {knowledge_drift['ado']}")
+            st.write(f"**Impact** -> {knowledge_drift['impact']}")
+            
+            st.divider()
+
+        st.subheader("✅ Suggested reviews for PM")
+        for review in drift["manual_review"]:
+            st.write(f"• {review}")
+
+        quality = drift["analysis_quality"]
+
+        st.subheader("📊 Analysis Quality")
+
+        st.metric(
+            "Confidence",
+            quality["confidence"]
+        )
+
+        st.caption(quality["reason"])
+    else:
+        st.error(f"Failed to generate AI summary. Error: {drift['error']}")
     
+
     ##########################################
-    # Display Confluence and Matched ADO
+    # Display Processed data
     ##########################################
+    st.subheader("Raw data")
+    with st.expander("Structured json output from llm"):
+        st.json(drift)
 
-    with col1:
-
-        st.subheader("Confluence")
-
-        st.write("### Feature")
-
-        st.write(confluence["title"])
-
-        st.write(f"Sections: {len(confluence['sections'])}")
-
-    with col2:
-        st.subheader("Selected ADO Feature")
-
-        st.write(feature["title"])
-
-        st.write(f"Stories: {len(feature['stories'])}")
+    with st.expander("Parsed artifacts data"):
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader("Confluence")
+            st.write("### Feature")
+            st.write(confluence["title"])
+            st.write(f"Sections: {len(confluence['sections'])}")
+            st.json(confluence)
+        with col2:
+            st.subheader("Selected ADO Feature")
+            st.write(feature["title"])
+            st.write(f"Stories: {len(feature['stories'])}")
+            st.write("##ADO all raw data")
+            st.json(ado)
 
 
---
+++ artifact_validator.py
@@ -0,0 +1,142 @@
+def validate_uploads(confluence, ado):
+    """
+    Validate uploaded artifacts before feature resolution.
+
+    For Confluence:
+    1. Should have a title that is to be matched with feature in ADO extract
+    2. Should have at least 3 sections that explains/defines the specs
+
+    For ADO:
+    1. There should be at least one FEATURE available
+    2. There be a feature available to select/match against the confluence
+    3. The selected feature should have at least 1 story
+    4. Each story under selected feature should have a Title, Desc and Acceptance Criteria
+
+    """
+
+    errors = []
+    warnings = []
+
+    ##################################################
+    # Confluence
+    ##################################################
+    print(f"\n\n Performing Confluence Validations \n\n")
+    print(f"\n\nConfluence page Title: {confluence.get('title')}")
+    if not confluence.get("title"):
+        errors.append("Confluence feature title not found.")
+
+    if not confluence.get("sections"):
+        errors.append("No sections found in Confluence page.")
+
+    elif len(confluence["sections"]) < 3:
+        warnings.append(
+            "Confluence contains very few sections."
+        )
+
+    ##################################################
+    # ADO
+    ##################################################
+
+    if not ado.get("features"):
+        errors.append("No Features found in ADO extract.")
+
+    elif len(ado["features"]) > 1:
+        warnings.append(
+            f"{len(ado['features'])} Features detected in ADO extract."
+        )
+
+    if ado["orphan_stories"]:
+        warnings.append(
+            f"{len(ado['orphan_stories'])} orphan Stories detected."
+        )
+
+    ##################################################
+
+    return True, {
+
+        "errors": errors,
+
+        "warnings": warnings
+
+    }
+
+
+def validate_selected_feature(feature):
+    """
+    Validate only the feature selected for comparison.
+    """
+
+    errors = []
+    warnings = []
+
+    ##################################################
+    # Feature
+    ##################################################
+
+    if feature is None:
+
+        errors.append("No ADO Feature selected.")
+
+        return True, {
+
+            "errors": errors,
+
+            "warnings": warnings
+
+        }
+
+    ##################################################
+    # Stories
+    ##################################################
+
+    if len(feature["stories"]) == 0:
+
+        errors.append(
+            "Selected Feature contains no Stories."
+        )
+
+    elif len(feature["stories"]) == 1:
+
+        warnings.append(
+            "Selected Feature contains only one Story."
+        )
+
+    titles = set()
+
+    for story in feature["stories"]:
+        print(f"Story Validation: {story}")
+        if not story["title"]:
+
+            errors.append(
+                f"Story {story['ticket_id']} has no title."
+            )
+
+        if not story["description"]:
+
+            warnings.append(
+                f"Story {story['ticket_id']} has no description."
+            )
+
+        if not story["acceptance_criteria"]:
+
+            warnings.append(
+                f"Story {story['ticket_id']} has no Acceptance Criteria."
+            )
+
+        if story["title"] in titles:
+
+            warnings.append(
+                f"Duplicate Story title: {story['title']}"
+            )
+
+        titles.add(story["title"])
+
+    ##################################################
+
+    return True, {
+
+        "errors": errors,
+
+        "warnings": warnings
+
+    }
